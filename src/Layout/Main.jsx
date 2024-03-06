5c548fb27a150633fa08a271cf3d614b0e18a71d
selim121 <selimkhan.bdkhan@gmail.com>
2024-03-06 22:15:54 +0600

    navbar design start

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Outlet, ScrollRestoration } from "react-router-dom";
+import Navbar from '../components/Navbar/Navbar';
 
 const Main = () => {
     return (
-        <div>
+        <div className='container mx-auto bg-[#070717]'>
+            {/* >>>> Navbar <<<< */}
+            <Navbar />
             <Outlet></Outlet>
             <ScrollRestoration />
         </div>
